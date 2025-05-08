89de46ce814ca601e66da2ca4a0e3ced9399d1ea
Roshan Kikani <roshankikani1718@gmail.com>
2025-05-08 18:33:02 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Machine learning Project that predicts food delivery time',
+    author='Roshan Kikani',
+    license='',
+)
